c76856ca20bb0af81bbaaf6336952734f46299a3
Marlon J. Frausto <marlonjfrausto@gmail.com>
2017-02-04 17:00:53 -0800

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require easypiechart.min
+//= require isotope.min
+//= require lightbox.min
+//= require owl.carousel.min
+//= require scrollreveal.min
+//= require smooth-scroll.min
+//= require twitterfetcher.min
+//= require ytplayer.min
+//= require scripts
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+  def petition_page
+  end
+  def creations_page
+  end
+  
+end
